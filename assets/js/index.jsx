9e7c241cacbd1569e208dc6c19a0eabba527b7bf
SudoBobo <ivan.koptelov@phystech.edu>
2017-12-04 11:47:01 +0300

    make use of plugins and NODE_ENV

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {printText, test} from './utils'
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
+
+printText(test)
+printText(test)
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,25 @@ var path = require("path")
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
+
     context: __dirname,
+    // context: `${__dirname}/asserts/js`,
 
     entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
 
     output: {
         path: path.resolve('./assets/bundles/'),
-        filename: "[name]-[hash].js",
+        filename: NODE_ENV === 'development' ? '[name].js' : '[name]-[hash].js',
+
     },
 
     plugins: [
         new BundleTracker({filename: './webpack-stats.json'}),
+        new webpack.NoEmitOnErrorsPlugin(),
     ],
 
     module: {
@@ -32,4 +39,17 @@ module.exports = {
         modules: ['node_modules', 'bower_components'],
         extensions: ['.js', '.jsx']
     },
+
+    watch: true
+};
+
+if (NODE_ENV !== 'development') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+            }
+        })
+    )
 }
